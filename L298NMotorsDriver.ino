30d6f3e73423b1701c8ef86df444355ab0b5bb29
Class2218 <68412859+Merron-cmd@users.noreply.github.com>
2026-04-02 22:20:20 +0800

    Created two headers
    Pins of motors defined in "motorPins.h", the functions in "motor.h"

--- L298NMotorsDriver.ino
+++ L298NMotorsDriver.ino
@@ -1,21 +1,5 @@
-/*
-Motor port definition
-*/
-#define Motor1_0 3
-#define Motor1_1 5
-
-
-/*
-PWM interval
-*/
-#define motor_period 5
-
-/*
-==========L298N Functions==========
-*/
-
-void setPWM(uint8_t port, uint8_t pwm);
-void setPWMTime(uint8_t port, uint8_t pwm, int ms);
+#include "motorPins.h"
+#include "motor.h"
 
 void setup() 
 {
@@ -39,35 +23,4 @@ void setup()
 void loop()
 {
 
-}
-
-void setPWM(uint8_t port, uint8_t pwm)
-{
-  pinMode(port, OUTPUT);
-  pwm = (pwm < 256) ? pwm : 255;
-  digitalWrite(port, HIGH);
-  int i;
-  for(i = 0; i < pwm; i ++)
-  {
-    delayMicroseconds(motor_period);
-  }
-  digitalWrite(port, LOW);
-  for(i = pwm; i < 255; i ++)
-  {
-    delayMicroseconds(motor_period);
-  }
-}
-
-void setPWMTime(uint8_t port, uint8_t pwm, int ms)
-{
-  int time = micros();
-  while(ms)
-  {
-    setPWM(port, pwm);
-    if(micros() - time >= 1000)
-    {
-      time = micros();
-      ms --;
-    }
-  }
 }
---
+++ motorPins.h
@@ -0,0 +1,5 @@
+/*
+Motor port definition
+*/
+#define Motor1_0 3
+#define Motor1_1 5
